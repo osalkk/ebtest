ce267c6b286b48320c969b6955052e6f1fd0c88b
Onur Salk <salk.onur@gmail.com>
2017-01-12 16:04:03 +0300

    changed endpoint

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config['MYSQL_DATABASE_USER'] = 'admin'
 app.config['MYSQL_DATABASE_PASSWORD'] = '1234qwer'
 app.config['MYSQL_DATABASE_DB'] = 'Bucket'
-app.config['MYSQL_DATABASE_HOST'] = 'aa1cab2pi9ru8j6.c3scnwvvpwfi.eu-west-2.rds.amazonaws.com'
+app.config['MYSQL_DATABASE_HOST'] = '1234.asd'
 mysql.init_app(app)
 
 
